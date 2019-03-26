d2c80d9343894b90b23e408a4d3080fb57e311a4
Taiwoo Park <taiwoo.park@gmail.com>
2019-03-25 21:19:58 -0700

    updates

--- build_pdf.bat
+++ build_pdf.bat
@@ -6,7 +6,6 @@ site\overview\index.html ^
 site\textbook\index.html ^
 site\grading\index.html ^
 site\classroom\index.html ^
-site\outcomes\index.html ^
 spring2019csc1130syl.pdf
 copy /Y spring2019csc1130syl.pdf ..\..\ 
 pause
